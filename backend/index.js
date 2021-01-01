347c20babfb152498864a1126b690679ccbd0eed
Vitor Hingel <vhahingel@hotmail.com>
2021-01-01 19:08:57 -0300

    Defined Color Theme, Cleaned FrontEnd and Configured Express

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.API_PORT || 10000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running at localhost: ${port}`);
+});
---
+++ frontend/src/theme/index.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: '#2a9d8f',
+    secondary: '#e76f51',
+  },
+});
+
+export default theme;
