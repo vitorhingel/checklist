7fa535f4b7bd3594957466c0d4ccf71680d2c004
Vitor Hingel <vhahingel@hotmail.com>
2021-01-03 18:57:05 -0300

    Added Custom Validator for Email (check duplicates in validation)

--- backend/validators/users.js
+++ backend/validators/users.js
@@ -1,7 +1,19 @@
 const { body, param } = require('express-validator');
+const validator = require('validator');
+const userModel = require('../models/users');
 
 const create = [
-  body('email').isEmail().withMessage('You must provide a valid email').normalizeEmail(),
+  body('email')
+    .custom(async (val, { req }) => {
+      if (val === undefined || !validator.isEmail('' + val)) return Promise.reject('You must provide a valid email');
+
+      const checkUser = await userModel.find({ email: val });
+
+      if (checkUser) return Promise.reject('This email is already registered');
+
+      return Promise.resolve();
+    })
+    .normalizeEmail(),
   body('password')
     .isLength({ min: 8 })
     .withMessage('Your password must have at least 8 characters')
@@ -22,7 +34,19 @@ const find = [param('id').optional().isInt({ min: 1 }).withMessage('You must pro
 
 const update = [
   param('id').isInt({ min: 1 }).withMessage('You must provide a valid ID').toInt(),
-  body('email').optional().isEmail().withMessage('You must provide a valid email').normalizeEmail(),
+  body('email')
+    .custom(async (val, { req }) => {
+      if (val === undefined) return Promise.resolve();
+
+      if (!validator.isEmail('' + val)) return Promise.reject('You must provide a valid email');
+
+      const checkUser = await userModel.find({ email: val });
+
+      if (checkUser) return Promise.reject('This email is already registered');
+
+      return Promise.resolve();
+    })
+    .normalizeEmail(),
   body('name')
     .optional()
     .not()
