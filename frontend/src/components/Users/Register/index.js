cde45bc9555e4715fe89e95dbebf383fcc1cb961
Hingel Vitor <vitor.hingel@orange.com>
2021-01-10 22:26:31 -0300

    Added Register Page [PENDING REGISTER REQUEST]

--- frontend/src/components/Users/Register/index.js
+++ frontend/src/components/Users/Register/index.js
@@ -3,20 +3,21 @@ import React, { useState } from 'react';
 import { Container, Grid, Typography, TextField, Box, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
-
 const useStyles = makeStyles((theme) => ({
-  wrapperLogin: {
+  wrapperRegister: {
     marginTop: 32,
     border: '1px solid #c4c4c4',
     padding: 20,
     borderRadius: 5,
   },
 }));
-const Login = () => {
+
+const Register = () => {
   const classes = useStyles();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -26,9 +27,9 @@ const Login = () => {
     <Container fluid>
       <Grid container spacing={2} direction='row' justify='center' alignItems='center'>
         <Grid item xs={12} md={4}>
-          <div className={classes.wrapperLogin}>
-            <Typography variant='h4'>Login</Typography>
-            <Typography variant='subtitle1'>Provide your details below</Typography>
+          <div className={classes.wrapperRegister}>
+            <Typography variant='h4'>Register</Typography>
+            <Typography variant='subtitle1'>Fulfill all details below</Typography>
             <Box marginTop={2} marginBottom={2}>
               <form onSubmit={onSubmit} noValidate>
                 <Grid container spacing={2}>
@@ -64,18 +65,30 @@ const Login = () => {
                     />
                   </Grid>
                   <Grid item xs={12}>
+                    <TextField
+                      required
+                      id='outlined-required'
+                      label='Confirm Password'
+                      variant='outlined'
+                      placeholder='Confirm Password...'
+                      type='password'
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      fullWidth
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
                     <Button variant='contained' color='primary' fullWidth type='submit'>
-                      Sign In
+                      Sign Up
                     </Button>
                   </Grid>
                   <Grid item xs={12}>
                     <Box textAlign='right'>
-                      <Link>
-                        <Typography variant='body2'>Don't know your password?</Typography>
-                      </Link>
-
-                      <Link to='/register'>
-                        <Typography variant='body2'>Create an account</Typography>
+                      <Link to='/'>
+                        <Typography variant='body2'>Already have an account?</Typography>
                       </Link>
                     </Box>
                   </Grid>
@@ -89,4 +102,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
--- frontend/src/routes/GeneralRoutes.js
+++ frontend/src/routes/GeneralRoutes.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import UsersLogin from '../components/Users/Login/';
-
+import UsersRegister from '../components/Users/Register/';
 const GeneralRoutes = () => {
   return (
     <Router>
       <Switch>
+        <Route path='/register'>
+          <UsersRegister />
+        </Route>
         <Route path='/'>
           <UsersLogin />
         </Route>
